a23a8866996009025b356047db4374a0ce688e91
SamuGG <SamuGG@users.noreply.github.com>
2022-11-06 21:51:30 +0000

    Assembly version sample

---
+++ samples/assembly-version/src/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp;
+
+partial class Program
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060", Justification = "No args needed")]
+    static void Main(string[] args)
+    {
+        GetAssemblyVersionFromReflection();
+        GetAssemblyVersionFromSourceGenerator();
+    }
+
+    private static void GetAssemblyVersionFromReflection()
+    {
+        var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+        var fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+        Console.WriteLine($"Version from reflection: {fvi.FileVersion}");
+    }
+
+    static partial void GetAssemblyVersionFromSourceGenerator();
+}
